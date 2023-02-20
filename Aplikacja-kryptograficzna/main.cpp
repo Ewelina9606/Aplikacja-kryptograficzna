71b6c57201d861182cd6b5f7ca745835255c874b
Ewelina <ewelinakolba00@gmail.com>
2023-02-20 17:57:28 +0100

    Celowe wprowadzenie błędu w kodzie na potrzeby Symulacji nr 2

--- Aplikacja-kryptograficzna/main.cpp
+++ Aplikacja-kryptograficzna/main.cpp
@@ -61,8 +61,7 @@ int main(int argc, char* argv[]) {
 				} while (d == -1);
 
 				std::cout << "Podaj wiadomosc: ";
-				std::cin.ignore(std::numeric_limits < std::streamsize >::max(), '\n');
-				std::getline(std::cin, message);
+				std::cin >> message;
 				std::cout << std::endl;
 
 				//------------------------------------------------------------------------------
